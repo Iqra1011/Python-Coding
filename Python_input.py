eceb8d97ef5b8827c26b20c2392c671246e8d954
Iqra Rehmat Ali <146968006+Iqra1011@users.noreply.github.com>
2024-07-24 21:02:44 +0500

    Create Python_input.py

---
+++ Python_input.py
@@ -0,0 +1,23 @@
+
+#x=10  #creating variable
+#print(x)
+
+#name= "amirah"
+#print(name) #print any data
+
+age= float(input("enter your age :")) #floatis a decimal number.
+name= input("enter your name : ") #getting user input
+print(name,age*5,age) #print any data
+
+age= int(input("enter your age :"))  #int is a whole number.
+print(age)
+
+#age= input("enter your age :")
+#print(age)
+#3 main data types:
+#1. string: sequence of character inside double, triple and single quotes.
+#2. integer: only numeric and without decimal point.
+#3. float : only with decimal numbers like 3,5,6,8.
+#age=input("enter your age")
+#name=input("enter your name")
+#print(age,name)
